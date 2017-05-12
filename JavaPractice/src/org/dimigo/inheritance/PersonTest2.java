7fb515b201253994ccd691ef8e098a26f89cb45c
parkjunmoa <parkjunmoa@naver.com>
2017-05-12 09:11:54 +0900

    asd

---
+++ JavaPractice/src/org/dimigo/inheritance/PersonTest2.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *    |_PersonTest
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 5. 8.
+ * </pre>
+ * 
+ * @AUTHOR : USER
+ * @VERSION : 1.0
+ */
+public class PersonTest2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Person[] p={
+			new Person("Tom"),
+			new Korean("홍길동"),
+			new Japanese("다나카"),
+			new Chinese("왕밍")
+		};
+		
+		for(Person i:p){
+			System.out.println(i);
+			i.sayHello();
+			i.sayBye();
+			System.out.println();
+		}
+	}
+}
